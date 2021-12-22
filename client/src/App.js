8c0d52c347300376b275b83213bc90ba513b38d3
LittleBiber <rlatkdgud98@naver.com>
2021-12-22 23:03:33 +0900

    backup

--- client/src/App.js
+++ client/src/App.js
@@ -27,29 +27,29 @@ export default function App () {
     console.log(accToken)
   }
 
-  // const isAuthenticated = () => {
-  //   axios
-  //     .get("https://localhost:3000") 
-  //     .then((res) => {
-  //       setIsLogin(true); 
-  //       setUserinfo(res.data.data.userInfo); 
-  //       history.push("/");
-  //     })
-  //     .catch((err) => {
-  //       console.log("토큰이 만료 되었습니다. 다시 로그인 해 주세요.")
-  //     });
-  //   // TODO: 이제 인증은 성공했습니다. 사용자 정보를 호출하고, 이에 성공하면 로그인 상태를 바꿉시다.
-  // };
-  // const handleResponseSuccess = () => {
-  //   isAuthenticated();
-  // };
-  // const handleLogout = () => {
-  //   axios.post('https://localhost:3000/signout').then((res) => {
-  //     setUserinfo(null);
-  //     setIsLogin(false);
-  //     history.push('/');
-  //   });
-  // };
+  const isAuthenticated = () => {
+    axios
+      .get("https://localhost:8443") 
+      .then((res) => {
+        setIsLogin(true); 
+        setUserinfo(res.data.data.userInfo); 
+        history.push("/");
+      })
+      .catch((err) => {
+        console.log("토큰이 만료 되었습니다. 다시 로그인 해 주세요.")
+      });
+    // TODO: 이제 인증은 성공했습니다. 사용자 정보를 호출하고, 이에 성공하면 로그인 상태를 바꿉시다.
+  };
+  const handleResponseSuccess = () => {
+    isAuthenticated();
+  };
+  const handleLogout = () => {
+    axios.post('https://localhost:3000/signout').then((res) => {
+      setUserinfo(null);
+      setIsLogin(false);
+      history.push('/');
+    });
+  };
 
   // useEffect(() => {
   //   isAuthenticated();
@@ -61,7 +61,7 @@ export default function App () {
       <Routes>
         <Route path='/login'element={<Login setToken={handleAccessToken} handleLogin={handleLogin}/>} />
         <Route path='/'element={<Main/>} />
-        <Route path='/mypage'element={<Mypage />} />
+        <Route path='/mypage'element={<Mypage accessToken={accessToken} />} />
         <Route path='/event'element={<Event/>} />
         <Route path='/ranking'element={<Ranking/>} />
         <Route path='/signin'element={<Signin/>} />
